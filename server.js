16aa550ad373d345c822de749b272cfcdfe40256
Marcus <markysalinas12@gmail.com>
2017-03-31 14:21:05 -0500

    -Added array to keep track of clients -set up socket in game.js

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-//var http = require("http")
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
 var path = require('path');
 //var server = http.createServer(app);
-//var io = require("socket.io").listen(server);
+var io = require('socket.io')(http);
 //var serverPacket = require("./serverPacket");
+
+var clients = {};
+var clientCount = 0;
 
 app.use(express.static(path.join(__dirname, '/client')));
 
@@ -13,4 +16,19 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/client/main.html'));
 });
 
-app.listen(8080);
+io.on('connection', function(socket){
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+        clientCount-=1;
+        delete clients[socket.id];
+    });
+
+    console.log('a user connected');
+    clientCount+=1;
+    clients[socket.id] = clientCount;
+
+});
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
